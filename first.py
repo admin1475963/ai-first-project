425272ea56f9433ae0af179f531e9eadcdc88bfb
Mukhammad Ilkhomov <ilkhomovmukhammad8128@gmail.com>
2025-02-07 15:40:33 +0500

    Change models to OpenAI

--- first.py
+++ first.py
@@ -1,8 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader, CSVLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_google_vertexai import VertexAIEmbeddings, ChatVertexAI
 from langchain_chroma import Chroma
 from langchain_core.prompts import PromptTemplate
 from typing_extensions import List, TypedDict
@@ -22,24 +24,26 @@ doc2 = doc2_loader.load()
 
 # split text
 csv_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=200, chunk_overlap=0, add_start_index=True
+    chunk_size=200, chunk_overlap=20, add_start_index=True
 )
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500, chunk_overlap=20, add_start_index=True
+    chunk_size=500, chunk_overlap=50, add_start_index=True
 )
 card_splits = csv_splitter.split_documents(cards)
 doc1_splits = text_splitter.split_documents(doc1)
 doc2_splits = text_splitter.split_documents(doc2)
 
-# embed documents
-embeddings = HuggingFaceEmbeddings(model_name="ai-forever/sbert_large_nlu_ru")
-vector_store = Chroma(embedding_function=embeddings, persist_directory="./vdb")
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
-_ = vector_store.add_documents(documents=card_splits)
+# embed documents
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+vector_store = Chroma(embedding_function=embeddings, persist_directory="./vdb")
+#_ = vector_store.add_documents(documents=card_splits)
 print("Cards done")
-_ = vector_store.add_documents(documents=doc1_splits)
+#_ = vector_store.add_documents(documents=doc1_splits)
 print("doc1 done")
-_ = vector_store.add_documents(documents=doc2_splits)
+#_ = vector_store.add_documents(documents=doc2_splits)
 print("doc2 done")
 
 
@@ -47,24 +51,24 @@ print("doc2 done")
 prompt = PromptTemplate.from_template(
     """
     Ты помощник-ассистент, который отвечает на вопросы клиента.
-    Используй следующий контекст для информации и ответь на вопрос клиента.
+    Отвечай используя только следующий контекст.
     Если ты не знаешь или не нашел ответа, так и скажи.
-    Если ты нашел больше одной информации, то подробно скажи о каждой из них,
-    но пропусти похожую но не связанную информацию.
+    Если ты нашел несколько ответов, то расскажи о каждой из них.
     Вопрос: {question} 
     Контекст: {context} 
     Ответ:
     """
 )
 
-llm = ChatOllama(model="llama3.1:8b")
+llm = ChatOpenAI(model="gpt-4o-mini")
 # define state for application
 class State(TypedDict):
     question: str
     context: List[Document]
     answer: str
 
-# retieve info from vector store
+
+# Define application steps
 def retrieve(state: State):
     retrieved_docs = vector_store.similarity_search(state["question"])
     for doc in retrieved_docs:
@@ -72,14 +76,15 @@ def retrieve(state: State):
         print(f"{doc.page_content}\n")
     return {"context": retrieved_docs}
 
-# generate ansver
+
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
     messages = prompt.invoke({"question": state["question"], "context": docs_content})
     response = llm.invoke(messages)
     return {"answer": response.content}
 
-# create app with langgraph
+
+# Compile application and test
 graph_builder = StateGraph(State).add_sequence([retrieve, generate])
 graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
